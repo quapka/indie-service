744ac3c9b0787bd451b1b06cbbb2f27f6d6ba37a
quapka <>
2024-11-11 15:51:44 +0100

    Use Flakes for development

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Indistinguishability Service";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    custom-nixpkgs.url = "github:quapka/nixpkgs/customPkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      custom-nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        customPkgs = import custom-nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        packages = rec { };
+        devShells.default = mkShell {
+          buildInputs = [ python312 ] ++ (with pkgs.python312Packages; [ cryptography ]);
+        };
+      }
+    );
+}
