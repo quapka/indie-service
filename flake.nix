4675758ac0088132bdc1add33eac822af62b0b2f
quapka <>
2025-10-23 10:36:41 +0200

    Update Nix env

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Indistinguishability Service";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-24_05.url = "github:NixOS/nixpkgs/24.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils";
     custom-nixpkgs.url = "github:quapka/nixpkgs/customPkgs";
   };
@@ -25,27 +25,57 @@
         # pkgs-24_05 = import nixpkgs-24_05 { inherit system overlays; };
         customPkgs = import custom-nixpkgs { inherit system overlays; };
         # pyscard-2_0_9 = pkgs-24_05.python312Packages.pyscard;
+        # myPython = pkgs.python3;
+        # pythonWithPkgs = myPython.withPackages (pythonPkgs: with pythonPkgs; [
+        #     pytest
+        #     # sage
+        #     ipython
+        #     pyscard
+        #     # pyscard-2_0_9
+        #   # ]);
+
+        #   # buildInputs =
+        #   #   [
+        #   #     python312
+        #   #   ]
+        #   #   ++ (with pkgs.python312Packages; [
+        #       cryptography
+        #       # secp256k1
+        #       fastecdsa
+        #       flake8
+        #       flask
+        #       noiseprotocol
+        #       pyjwt
+        #       mypy
+        #       black
+        # ]);
       in
       with pkgs;
       {
         packages = rec { };
         devShells.default = mkShell {
-          nativeBuildInputs = [
-            nodejs
-            sage
-          ] ++ (with pkgs.python312Packages; [
+          # python = python3;
+
+          buildInputs = [
+            # pythonWithPkgs
+            # python3Packages.pyscard
+
+            # nodejs
+            # sage
+          ]
+          ++ (with python3Packages; [
             pytest
-            sage
+            # sage
             ipython
             pyscard
             # pyscard-2_0_9
-          ]);
+          # ]);
 
-          buildInputs =
-            [
-              python312
-            ]
-            ++ (with pkgs.python312Packages; [
+          # buildInputs =
+          #   [
+          #     python312
+          #   ]
+          #   ++ (with pkgs.python312Packages; [
               cryptography
               # secp256k1
               fastecdsa
